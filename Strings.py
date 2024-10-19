8146407ba7ad4812f1d45dc1a0199ec82e3ede5f
Chhatra_Ram <178591323+ChhatraD@users.noreply.github.com>
2024-10-19 22:11:13 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,20 @@
+
+# --Data types in Python
+
+# Integers
+# string 
+# float
+# boolen
+# None
+# list
+# tuple
+# set 
+# dict
+
+
+a = 45
+b= 49
+
+print("hellow world")
+print(type(b))
+print(a+b)
